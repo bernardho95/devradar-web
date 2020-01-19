9fd8a174ade77d63aacfbec2c76ee531a845538f
Álvaro <alvarojfjunior@gmail.com>
2020-01-19 20:21:57 -0200

    Updates

---
+++ precache-manifest.57d81daa3a9adb4a879c1332036e1525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3ec2c40c60b8850562c6605a16ccd3",
+    "url": "/devradar-web/index.html"
+  },
+  {
+    "revision": "2b1bb30bc9d370a302e8",
+    "url": "/devradar-web/static/css/main.6426cee2.chunk.css"
+  },
+  {
+    "revision": "e19ded47508c791c279d",
+    "url": "/devradar-web/static/js/2.733b4ed7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/devradar-web/static/js/2.733b4ed7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b1bb30bc9d370a302e8",
+    "url": "/devradar-web/static/js/main.041b9064.chunk.js"
+  },
+  {
+    "revision": "f0b3efb3a3438811b007",
+    "url": "/devradar-web/static/js/runtime-main.2f674770.js"
+  }
+]);
--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    //baseURL: 'http://localhost:3333'
-    baseURL: 'https://api-devradar.herokuapp.com'
-});
-
-export default api;
